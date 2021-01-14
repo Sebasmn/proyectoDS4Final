9e867227ced786436609e0686f1d0cfe53ad554f
Sebastian Mayorga <sebasmayorga98@gmail.com>
2021-01-14 16:28:04 -0500

    Agregar archivos de proyecto.

---
+++ CreacionDocumentoDemo/Formulario/ValidacionDatos.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ValidacionDatos.aspx.cs" Inherits="CreacionDocumentoDemo.Formulario.ValidacionDatos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-bottom: 0px;
+        }
+        .auto-style2 {
+            width: 902px;
+            height: 426px;
+            margin-left: 40px;
+        }
+    </style>
+    <link rel="stylesheet" href="../Estilos/estilos.css"  type="text/css" />
+</head>
+    
+<body>
+    <form id="form1" runat="server">
+        <div id="container" class="auto-style2">
+            <asp:Image ID="Image1"  runat="server" Height="105px" ImageAlign="TextTop" ImageUrl="~/Images/EscudoUTA.png" Width="100px" />
+            <br />
+            <h1>Facultad de Ingeniería en Sistemas, Electrónica e Industrial</h1>
+            <h2>Sistema de Manejo de Archivos</h2>
+            <hr />
+            <br />
+            <div id="controles">
+                <asp:TextBox ID="TextBox2" runat="server" placeholder="Nombres"></asp:TextBox>
+                <asp:TextBox ID="TextBox1" runat="server"  placeholder="Fecha"  CssClass="auto-style1" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+                  <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Buscar Datos" Width="143px" />
+            </div>
+            <br />
+        </div>
+      
+    </form>
+</body>
+</html>
---
+++ CreacionDocumentoDemo/Formulario/ValidacionDatos.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Word = Microsoft.Office.Interop.Word;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Reflection;
+using System.IO;
+using Microsoft.Office.Interop.Word;
+
+namespace CreacionDocumentoDemo.Formulario
+{
+    public partial class ValidacionDatos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb1 = new StringBuilder();
+            sb1.Append(@"C:\Users\franc\Documents\Oficios\");
+                
+           /* SqlConnection sqlConnection = new SqlConnection(Properties.Settings.Default.conecta);
+            sqlConnection.Open();
+            SqlCommand sql = new SqlCommand();
+            sql.Connection = sqlConnection;
+            string sqlconsulta = "SELECT NEXT VALUE FOR Conteo";
+            sql.CommandText = sqlconsulta;
+            int conteo = 0;
+            using (var dr = sql.ExecuteReader())
+            {
+                if (dr.Read())
+                {
+                    conteo = Convert.ToInt32(dr[0]);
+                }
+            }*/
+            StringBuilder sb2 = new StringBuilder();
+          //  sb2.Append(textBox_Nombre.Text);
+            sb2.Append("Uno");
+            sb1.Append(sb2.ToString());
+            sb1.Append(".docx");
+            CreateWordDocument(@"C:\Users\franc\Documents\Oficios\plantilla.docx", sb1.ToString());
+        
+    }
+
+        private void CreateWordDocument(object filename, object SaveAs)
+        {
+            Word.Application wordApp = new Word.Application();
+            object missing = Missing.Value;
+            Word.Document myWordDoc = null;
+
+            if (File.Exists((string)filename))
+            {
+                object readOnly = false;
+                object isVisible = false;
+                wordApp.Visible = false;
+
+                myWordDoc = wordApp.Documents.Open(ref filename, ref missing, ref readOnly,
+                                        ref missing, ref missing, ref missing,
+                                        ref missing, ref missing, ref missing,
+                                        ref missing, ref missing, ref missing,
+                                        ref missing, ref missing, ref missing, ref missing);
+                myWordDoc.Activate();
+
+                //find and replace
+                this.FindAndReplace(wordApp, "<fecha>", TextBox1.Text);
+                this.FindAndReplace(wordApp, "<nombre>", TextBox2.Text);
+               
+                /* this.FindAndReplace(wordApp, "<name>", textBox_Nombre.Text);
+                 this.FindAndReplace(wordApp, "<id>", textBox_Cedula.Text);
+                 this.FindAndReplace(wordApp, "<level>", textBox_Nivel.Text);
+                 this.FindAndReplace(wordApp, "<carrer>", textBox_Carrera.Text);
+                 this.FindAndReplace(wordApp, "<course>", comboBox_Materia.SelectedItem.ToString());
+                 this.FindAndReplace(wordApp, "<birthday>", dateTimePicker1.Value.ToShortDateString());
+                 this.FindAndReplace(wordApp, "<phone>", textBox_Celular.Text);
+                 this.FindAndReplace(wordApp, "<mail>", textBox_Correo.Text);
+                 this.FindAndReplace(wordApp, "<facultad>", facultad);*/
+
+
+            }
+            else
+            {
+                //MessageBox.Show("File not Found!");
+            }
+
+            //Save as
+            myWordDoc.SaveAs(ref SaveAs, ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing);
+
+            myWordDoc.Close();
+            wordApp.Quit();
+          //  MessageBox.Show("Archivo Creado!");
+        }
+
+        private void FindAndReplace(Word.Application wordApp, object ToFindText, object replaceWithText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundLike = false;
+            object nmatchAllforms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiactitics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+
+            wordApp.Selection.Find.Execute(ref ToFindText,
+                ref matchCase, ref matchWholeWord,
+                ref matchWildCards, ref matchSoundLike,
+                ref nmatchAllforms, ref forward,
+                ref wrap, ref format, ref replaceWithText,
+                ref replace, ref matchKashida,
+                ref matchDiactitics, ref matchAlefHamza,
+                ref matchControl);
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
