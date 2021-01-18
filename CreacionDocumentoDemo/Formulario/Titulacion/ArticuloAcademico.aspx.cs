78f5f4cc69d1e9af86f05a2e5c35b84665412302
Sebastian Mayorga <sebasmayorga98@gmail.com>
2021-01-18 12:26:44 -0500

    Formato articulo academico

---
+++ CreacionDocumentoDemo/Formulario/Titulacion/ArticuloAcademico.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CreacionDocumentoDemo.Formulario.Titulacion
+{
+    public partial class ArticuloAcademico : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                cargarDatos();
+            }
+        }
+
+        private void cargarDatos()
+        {
+            ManejoDatos md = new ManejoDatos();
+
+            //Sesion
+            List<string> sesion = new List<string>();
+            sesion.Add("Ordinaria");
+            sesion.Add("Extraordinaria");
+            ddlSesion.DataSource = sesion;
+            ddlSesion.DataBind();
+
+
+            //Nombres de Dias
+            List<string> dias = md.ObtenerDiasSemana();
+            ddlNombreDia.DataSource = dias;
+            ddlNombreDia.DataBind();
+
+
+            //Dias del mes
+            List<string> numerosMes = md.ObtenerDiasMes();
+            ddlNumeroDia.DataSource = numerosMes;
+            ddlNumeroDia.DataBind();
+
+            ddlNumeroDia0.DataSource = numerosMes;
+            List<int> numeros = new List<int>();
+            for (int i = 1; i < 32; i++)
+            {
+                numeros.Add(i);
+            }
+            ddlNumeroDia.DataSource = numeros;
+            ddlNumeroDia.DataBind();
+
+            ddlNumeroDia0.DataSource = numeros;
+            ddlNumeroDia0.DataBind();
+
+            //Meses
+            List<string> meses = md.ObtenerMeses();
+
+            ddlMes.DataSource = meses;
+            ddlMes.DataBind();
+            ddlMes0.DataSource = meses;
+            ddlMes0.DataBind();
+
+            //Carreras
+            ManejoDatos datos = new ManejoDatos();
+            List<string> modelo = datos.getCarreras();
+            List<string> modeloMin = datos.GetCarrerasMinuscula();
+            List<string> carrerasF = datos.GetCarrerasFooter();
+            ddlCarrera.DataSource = modeloMin;
+            ddlCarrera.DataBind();
+            ddlCarrera0.DataSource = modelo;
+            ddlCarrera0.DataBind();
+            ddlCarreras.DataSource = carrerasF;
+            ddlCarreras.DataBind();
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnNumeroResolucion_Click(object sender, EventArgs e)
+        {
+            generarNumeroResolucion();
+        }
+        private void generarNumeroResolucion()
+        {
+            ManejoDatos mysql = new ManejoDatos();
+            string resolucion = mysql.obtenerSiguienteResolución();
+            txtSecuencia.Text = resolucion;
+        }
+    }
+}
