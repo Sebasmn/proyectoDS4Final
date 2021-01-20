85badad1e41f5c416dadaffd6559197a784c0119
Sebastian Mayorga <sebasmayorga98@gmail.com>
2021-01-20 02:06:15 -0500

    cambios

---
+++ CreacionDocumentoDemo/Admin/CrearConsejo.aspx
@@ -0,0 +1,101 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CrearConsejo.aspx.cs" Inherits="CreacionDocumentoDemo.Admin.CrearConsejo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Actualización Conocimientos</title>
+       <link rel="stylesheet" href="~/Estilos/estilos.css"  type="text/css" />
+    <style type="text/css">
+        .auto-style1 {
+            left: -3px;
+            top: 0px;
+        }
+        .auto-style2 {
+            width: 100%;
+        }
+        .auto-style3 {
+            height: 432px;
+        }
+        .auto-style4 {
+            height: 43px;
+        }
+        .auto-style5 {
+            height: 41px;
+        }
+        .auto-style6 {
+            height: 52px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        <div id="formularioLogin" class="auto-style3">
+            <asp:Image ID="escudoLogin" runat="server" 
+                    ImageUrl="~/Images/EscudoUTA.png" CssClass="auto-style1"
+                    />
+            <h1>
+                Creación de Consejo
+                
+            </h1>
+            <hr />
+            <br />
+            <table class="auto-style2">
+                <tr>
+                    <td>
+            <asp:Label ID="Label1" runat="server" Font-Italic="True" Font-Names="Calisto MT" Font-Size="Medium" ForeColor="#785E37" Text="Ingrese código de consejo:"></asp:Label>
+                    </td>
+                    <td>
+            <asp:TextBox ID="TextBox1" runat="server" required="true" placeholder="CD001-2020" CssClass="inputLogin"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style4" colspan="2">
+                        <center>
+                             <br />
+                <asp:Label ID="Label2" runat="server" Font-Italic="True" Font-Names="Calisto MT" Font-Size="Medium" ForeColor="#785E37" Text="Seleccione fecha:"></asp:Label>
+                            <br />
+                            <br />
+                            <asp:Calendar ID="Calendar1" runat="server" Width="377px"></asp:Calendar>
+                            <br />
+                        </center>
+                       
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">
+            <asp:Label ID="Label5" runat="server" Font-Italic="True" Font-Names="Calisto MT" Font-Size="Medium" ForeColor="#785E37" Text="Ingrese cédula la persona que crea el consejo:"></asp:Label>
+                    </td>
+                    <td class="auto-style4">
+            <asp:TextBox ID="TextBox3" runat="server" required="true" placeholder="Ejm: 1804156112" CssClass="inputLogin" Height="34px" Width="177px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style6">
+            <asp:Label ID="Label3" runat="server" Font-Italic="True" Font-Names="Calisto MT" Font-Size="Medium" ForeColor="#785E37" Text="Ingrese cédula de persona encargada:"></asp:Label>
+                    </td>
+                    <td class="auto-style6">
+            <asp:TextBox ID="TextBox4" runat="server" required="true" placeholder="Ejm: 1804156112" CssClass="inputLogin" Height="35px" Width="179px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style5">
+            <asp:Label ID="Label4" runat="server" Font-Italic="True" Font-Names="Calisto MT" Font-Size="Medium" ForeColor="#785E37" Text="Ingrese clave:"></asp:Label>
+                    </td>
+                    <td class="auto-style5">
+            <asp:TextBox ID="TextBox5" runat="server" required="true" placeholder="*********" CssClass="inputLogin" Height="35px" Width="179px" TextMode="Password"></asp:TextBox>
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <br />
+            <asp:Button ID="btnCrear" runat="server" CssClass="btnBuscar" Text="Crear" OnClick="btnCrear_Click" />
+            <br />
+            <br />
+        </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CreacionDocumentoDemo/Admin/CrearConsejo.aspx.designer.cs
@@ -0,0 +1,134 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace CreacionDocumentoDemo.Admin
+{
+
+
+    public partial class CrearConsejo
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control escudoLogin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image escudoLogin;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control Calendar1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar Calendar1;
+
+        /// <summary>
+        /// Control Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
+        /// Control TextBox3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Control TextBox4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Control TextBox5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+
+        /// <summary>
+        /// Control btnCrear.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCrear;
+    }
+}
