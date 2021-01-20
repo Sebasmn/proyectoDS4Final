6cb531e3f30b3907e7db86434513fd753b06a3c8
Sebastian Mayorga <sebasmayorga98@gmail.com>
2021-01-19 21:05:01 -0500

    prorroga gratuita hecha

--- CreacionDocumentoDemo/Formulario/Titulacion/ProrrogaGratuita.aspx.cs
+++ CreacionDocumentoDemo/Formulario/Titulacion/ProrrogaGratuita.aspx.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -103,7 +104,89 @@ namespace CreacionDocumentoDemo.Formulario.Titulacion
 
         protected void Button4_Click(object sender, EventArgs e)
         {
+            /*Editables Y Datos*/
+            List<string> editables = new List<string>();
+            List<string> datos = new List<string>();
 
+            editables.Add("<fecha>"); datos.Add(txtFecha.Text);
+            editables.Add("<secuencia>"); datos.Add(txtSecuencia.Text);
+            editables.Add("<anio0>"); datos.Add(txtAnio.Text);
+
+            editables.Add("<coordinador>"); datos.Add(txtCoordinador.Text);
+            editables.Add("<carreras>"); datos.Add(ddlCarrerasCoor.SelectedValue.ToString());
+            editables.Add("<sesion>"); datos.Add(ddlSesion.SelectedValue.ToString());
+            editables.Add("<nombredia>"); datos.Add(ddlNombreDia.SelectedValue.ToString());
+
+
+            editables.Add("<numerodia>"); datos.Add(ddlDiaNum.SelectedValue.ToString());
+            editables.Add("<nombremes>"); datos.Add(ddlMes.SelectedValue.ToString());
+            editables.Add("<anio>"); datos.Add(txtAnio1.Text);
+
+            editables.Add("<acuerdo>"); datos.Add(txtAcuerdo.Text);
+            editables.Add("<nombremes1>"); datos.Add(ddlMes0.SelectedValue.ToString());
+            editables.Add("<numerodia1>"); datos.Add(ddlDiaNum0.SelectedValue.ToString());
+
+
+            editables.Add("<anio1>"); datos.Add(txtAnio2.Text);
+            editables.Add("<presidente>"); datos.Add(txtPresiConsejo.Text);
+            editables.Add("<estudiante>"); datos.Add(txtNombreEstu1.Text);
+
+            editables.Add("<carrera>"); datos.Add(ddlCarreras.SelectedValue.ToString());
+            editables.Add("<periodoacademico>"); datos.Add(txtPeriodo.Text);
+            editables.Add("<estudiante1>"); datos.Add(txtNombreEstu2.Text);
+            editables.Add("<periodoacademico1>"); datos.Add(txtPeriodo0.Text);
+            editables.Add("<carrera1>"); datos.Add(ddlCarreras0.SelectedValue.ToString());
+
+            editables.Add("<tematitulacion>"); datos.Add(txtTema.Text);
+            editables.Add("<resolucion>"); datos.Add(txtResolucion.Text);
+            editables.Add("<nombremes2>"); datos.Add(ddlMes1.SelectedValue.ToString());
+            editables.Add("<numerodia2>"); datos.Add(ddlDiaNum1.SelectedValue.ToString());
+            editables.Add("<anio2>"); datos.Add(txtAnio3.Text);
+            editables.Add("<memorando>"); datos.Add(txtMemorando.Text);
+            editables.Add("<numerodia3>"); datos.Add(ddlDiaNum3.Text);
+            editables.Add("<nombremes3>"); datos.Add(ddlMes3.SelectedValue.ToString());
+            editables.Add("<anio3>"); datos.Add(txtAnio5.Text);
+            editables.Add("<ingeniero>"); datos.Add(txtTutor.Text);
+            editables.Add("<estudiante2>"); datos.Add(txtNombreEstu3.Text);
+            editables.Add("<resolucion1>"); datos.Add(txtResolucion0.Text);
+            editables.Add("<nombremes4>"); datos.Add(ddlMes2.Text);
+            editables.Add("<numerodia4>"); datos.Add(ddlDiaNum2.Text);
+            editables.Add("<anio4>"); datos.Add(txtAnio4.Text);
+            editables.Add("<presidenta>"); datos.Add(txtPresidente.Text);
+            editables.Add("<presidente1>"); datos.Add(txtPresidente0.Text);
+            editables.Add("<tutor>"); datos.Add(txtTutor0.Text);
+            /**/
+            StringBuilder sb1 = new StringBuilder();
+            sb1.Append(@"D:\Documentos\Pruebas\");
+            StringBuilder sb2 = new StringBuilder();
+            sb2.Append("Resolucion");
+
+            sb2.Append(txtSecuencia.Text).Append("-P-CD-FISEI-UTA-").Append(txtAnio.Text);
+            StringBuilder codigo = new StringBuilder();
+            codigo.Append(txtSecuencia.Text).Append("-P-CD-FISEI-UTA-").Append(txtAnio.Text);
+            sb1.Append(sb2.ToString());
+            sb1.Append(".docx");
+            string resolucion = codigo.ToString();
+            string ruta = sb1.ToString();
+
+            String plantilla = @"D:\Documentos\OficiosPlantilla\Sistemas\Titulacion\ProrrogaGratuita.docx";
+            ManejoDatos mysql = new ManejoDatos();
+            Resolucion resol = new Resolucion();
+            resol.Ubicacion = ruta;
+            resol.Editables = editables;
+            resol.Datos = datos;
+            resol.Codigo = resolucion;
+            resol.Plantilla = plantilla;
+            resol.IDConsejo = txtCodigoConsejoDestino.Text;
+            resol.Estudiante = ViewState["CEDULA_EST"].ToString();
+            resol.Secretaria = ((UsuariosSW)Session["USUARIOSW"]).Cedula;
+
+            bool guardado = mysql.guardarResolucion(resol);
+
+            if (guardado)
+            {
+                labelEstado.Text = "Documento Generado y Guardado";
+            }
         }
 
         protected void Button3_Click(object sender, EventArgs e)
